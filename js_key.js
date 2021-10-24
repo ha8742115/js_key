8696de80ef48cd2492169b19c109925b2ba6b317
Hamidreza ahmadi <92933861+ha8742115@users.noreply.github.com>
2021-10-24 17:58:14 +0330

    Create js_key.js

---
+++ js_key.js
@@ -0,0 +1,3 @@
+    js_key = {
+        new_js_key : function (js_key_name,js_key_function,js_key_function_input){document.addEventListener('keydown', (event) => {if (event.key===js_key_name){js_key_function(js_key_function_input)}},false)}
+    }
